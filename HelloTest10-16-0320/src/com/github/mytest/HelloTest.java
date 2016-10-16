5d6361e22b1ca48fec6df5f3d6b5dc26b465de02
java2888 <2975370834@qq.com>
2016-10-17 00:20:42 +0800

    00:20 commit

--- HelloTest10-16-0320/src/com/github/mytest/HelloTest.java
+++ HelloTest10-16-0320/src/com/github/mytest/HelloTest.java
@@ -1,10 +1,10 @@
 package com.github.mytest;
 
 public class HelloTest {
-
+ 
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
-
+		System.out.println("Hello world.");
 	}
 
 }
