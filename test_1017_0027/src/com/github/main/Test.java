ca7d7c096f65915ebe8ea2b8e3f2ec704861b9a6
java2888 <2975370834@qq.com>
2016-10-17 20:19:13 +0800

    revision: t5->t6

--- test_1017_0027/src/com/github/main/Test.java
+++ test_1017_0027/src/com/github/main/Test.java
@@ -4,17 +4,17 @@ import com.github.run.Test4;
 import com.github.run.test3;
 
 public class Test {
- 
+
 	public static void main(String[] args) {
-		test2 t=new test2();
+		test2 t = new test2();
 		t.print();
-        test3 t3=new test3();
-        t3.run3();
-        Test4 t4=new Test4();
-        t4.Print();
-	t4.Print();
-	Test4 t5=new Test4();
-	t5.Print();
+		test3 t3 = new test3();
+		t3.run3();
+		Test4 t4 = new Test4();
+		t4.Print();
+		t4.Print();
+		Test4 t7 = new Test4();
+		t7.Print();
 	}
 
 }
